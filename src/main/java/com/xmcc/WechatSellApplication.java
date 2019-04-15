7868105775c3f18cb67bd8323831a84ed1ed9b93
cmxx <1049025904@qq.com>
2019-04-15 15:58:04 +0800

    第一次提交：测试1

---
+++ src/main/java/com/xmcc/WechatSellApplication.java
@@ -0,0 +1,13 @@
+package com.xmcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatSellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatSellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xmcc/wechat_sell/WechatSellApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xmcc.wechat_sell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WechatSellApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
